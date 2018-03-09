16319bd8b01e9944677d9f1ce5a278dc5b7697d3
sunil <sunil.yadav@rakuten.com>
2018-03-09 11:50:48 +0530

    1

---
+++ src/main/java/com/boot/test/springboot/BootApplication.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.boot.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author sunil.yadav
+ *
+ */
+@SpringBootApplication
+@ComponentScan(basePackages="com.boot.test.*")
+@EnableAutoConfiguration
+public class BootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+		LambdaTest.lambda();// lambda java 8 example here.
+		SumTwoNumbers numbers = (a, b) -> (a+b);
+		System.out.println(numbers.sum(1,  13));
+		print(numbers.sum(10, 15));
+	}
+	
+	private static void print(int item) {
+		System.out.println("sum is = " + item);
+	}
+}
---
+++ src/main/java/com/boot/test/springboot/LambdaTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.boot.test.springboot;
+
+/**
+ * @author sunil.yadav
+ *
+ */
+public class LambdaTest {
+
+	public static void lambda() {
+		//example one : pass away methods
+		Thread thread = new Thread(new Runnable() {
+			public void run() {
+				System.out.println("Old way!");
+			}
+		});
+		thread.start();
+		
+		Thread th = new Thread(() -> System.out.println("lambda way"));
+		th.start();
+	}
+}
