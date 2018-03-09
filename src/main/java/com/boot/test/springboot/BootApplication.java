ba6b8613d00c1ee212558d6d7022d148f75a2440
sunil <sunil.yadav@rakuten.com>
2018-03-09 22:29:26 +0530

    5

--- src/main/java/com/boot/test/springboot/BootApplication.java
+++ src/main/java/com/boot/test/springboot/BootApplication.java
@@ -6,6 +6,8 @@ package com.boot.test.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -15,7 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages="com.boot.test.*")
 @EnableAutoConfiguration
-public class BootApplication {
+public class BootApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
 		LambdaTest.lambda();// lambda java 8 example here.
@@ -23,6 +25,11 @@ public class BootApplication {
 		System.out.println(numbers.sum(1,  13));
 		print(numbers.sum(10, 15));
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BootApplication.class);
+    }
 	
 	private static void print(int item) {
 		System.out.println("sum is = " + item);
